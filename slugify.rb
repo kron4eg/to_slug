523a8eba0f4f13b1708db61fe86a794a20bc0a8a
Artiom Diomin <kron82@gmail.com>
2009-05-01 19:48:15 +0300

    typo %)

--- slugify.rb
+++ slugify.rb
@@ -4,7 +4,7 @@ module Slug
   def slugify
     trans_table = YAML.load_file("#{RAILS_ROOT}/config/trans_table.yml")
     str = self.mb_chars.downcase
-    str.each_char.map {|c| trans_table[c] or c == " " ? c : "-" }.join
+    str.each_char.map {|c| trans_table[c] or c == " " ? "-" : c }.join
   end
   alias :to_slug :slugify
 end
