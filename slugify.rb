56adbf6587929b40dd370ecabd252ce6090e24b8
Branko Vukelic <bg.branko@gmail.com>
2009-05-03 06:24:13 +0800

    Removed Slug module, moved the code to String class.
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- slugify.rb
+++ slugify.rb
@@ -1,14 +1,10 @@
 # encoding: utf-8
 
-module Slug
+class String
   def slugify
     trans_table = YAML.load_file("#{RAILS_ROOT}/config/trans_table.yml")
     str = self.mb_chars.downcase
     str.each_char.map {|c| trans_table[c] or c == " " ? "-" : c }.join.gsub(/[\W_]+/, "-")
   end
   alias :to_slug :slugify
-end
-
-class String
-  include Slug
 end
