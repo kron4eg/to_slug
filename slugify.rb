fe55ee9151edd88210c5dc61f33dba38ca9c7532
Branko Vukelic <bg.branko@gmail.com>
2009-05-03 18:16:47 +0800

    Added the `spacer` parameter.
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- slugify.rb
+++ slugify.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 class String
-  def slugify
+  def slugify(spacer="-")
     trans_table = YAML.load_file("#{RAILS_ROOT}/config/trans_table.yml")
     str = self.mb_chars.downcase
-    str.each_char.map {|c| trans_table[c] || c }.join.gsub(/[\W_]+/, "-")
+    str.each_char.map {|c| trans_table[c] || c }.join.gsub(/[\W_]+/, spacer)
   end
   alias :to_slug :slugify
 end
